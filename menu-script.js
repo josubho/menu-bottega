65e2656717cecae7c03265881eb3c7a0fbbfcad8
josubho <95227663+josubho@users.noreply.github.com>
2024-04-17 16:32:49 +0200

    second commit

--- menu-script.js
+++ menu-script.js
@@ -5,47 +5,54 @@ const comida_cena = [['alubias', 'pasta'], ['ensalada','patatas fritas'], ['pan'
 const comentarios = ['¡Muy bien elegido!', '¡Buena opción!', '¡Es muy nutritivo lo seleccionado!']
 var choice;
 var num_comentario;
-var precio_total;
+var precio;
 var seleccion;
 
 seleccion = true
+choice = prompt('Qué quieres desayuno, comida o cena?')
 while (seleccion) {
-    choice = prompt('Qué quieres desayuno, comida o cena?')
     if (tipo.includes(choice)){
         seleccion = false
+    } else {
+        choice = prompt('La opción es incorrecta. Debes introducir una de las opciones como aparece en la siguiente pregutna: ¿Qué quieres desayuno, comida o cena?')
     }
 }
 
 if (choice == 'desayuno') {
-    precio_total = 9.95
+    precio = 9.95
     for (var i=0; i <desayuno.length; i++ ){
         if (i==0) {
             seleccion = true
+            var primero = prompt(`Los platos principales son: ${desayuno[i][0]} o ${desayuno[i][1]} e incluyne dos acompañamientos por ${precio}€.`)
             while (seleccion) {
-                var primero = prompt(`Los platos principales son: ${desayuno[i][0]} o ${desayuno[i][1]}`)
                 if (desayuno[i].includes(primero)) {
                     seleccion = false
+                } else{
+                    primero = prompt(`La selección es incorrecta. Escribe la opción como aparece a continuación: ${desayuno[i][0]} o ${desayuno[i][1]}. Recuerde que le precio es de ${precio} e incluye dos acompañamientos.`)
                 }
             }   
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
-            alert('El precio del desayuno es 9,95€ con dos acompañamientos.')
         } else if (i==1){
             seleccion = true
+            var primer_acomp = prompt(`El primer acompañamiento puede ser: ${desayuno[i][0]} o ${desayuno[i][1]} y está incluido en el precio inicial de ${precio}€.`)
             while (seleccion) {
-                var primer_acomp = prompt(`El primer acompañamiento puede ser: ${desayuno[i][0]} o ${desayuno[i][1]}`)
                 if (desayuno[i].includes(primer_acomp)) {
                     seleccion = false
+                } else {
+                    primer_acomp = prompt(`La selección es incorrecta. Escribe una de las opciones como se muestra a continuación: ${desayuno[i][0]} o ${desayuno[i][1]} y está incluido en el precio inicial de ${precio}€..`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
         } else {
             seleccion = true
+            var segundo_acomp = prompt(`El segundo acompañamiento puede ser: ${desayuno[i][0]} o ${desayuno[i][1]} y está incluido en el precio inicial de ${precio}€.`)
             while (seleccion) {
-                var segundo_acomp = prompt(`El segundo acompañamiento puede ser: ${desayuno[i][0]} o ${desayuno[i][1]}`)
                 if (desayuno[i].includes(segundo_acomp)) {
                     seleccion = false
+                } else {
+                    segundo_acomp = prompt(`La selección es incorrecta. Escribe lo mismo que una de las siguientes opciones: ${desayuno[i][0]} o ${desayuno[i][1]} y está incluido en el precio inicial de ${precio}€.`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
@@ -53,35 +60,40 @@ if (choice == 'desayuno') {
         }
     }
 } else if (choice == 'comida') {
-    precio_total = 17.95
+    precio = 17.95
     for (var i=0; i <comida_cena.length; i++ ){
         if (i==0) {
             seleccion = true
-            while (seleccion) {
-                var primero = prompt(`Los platos principales son: ${comida_cena[i][0]} o ${comida_cena[i][1]}`)
+            var primero = prompt(`Los platos principales son: ${comida_cena[i][0]} o ${comida_cena[i][1]} e incluyen dos acompañamientos por un precio total de ${precio}€.`)
+            while (seleccion) {  
                 if (comida_cena[i].includes(primero)) {
                     seleccion = false
+                } else {
+                    primero = prompt(`La selección es incorrecta. Elige una de las siguientes dos opciiones: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
-            alert('El precio de la comida es 17,95€ con dos acompañamientos.')
         } else if (i==1){
             seleccion = true
+            var primer_acomp = prompt(`El primer acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
             while (seleccion) {
-                var primer_acomp = prompt(`El primer acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]}`)
                 if(comida_cena[i].includes(primer_acomp)) {
                     seleccion = false
+                } else {
+                    primer_acomp = prompt(`No es correcta la selección. Elige una de estas dos opcines: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
         } else {
             seleccion = true
+            var segundo_acomp = prompt(`El segundo acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
             while (seleccion) {
-                var segundo_acomp = prompt(`El segundo acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]}`)
                 if(comida_cena[i].includes(segundo_acomp)) {
                     seleccion = false
+                } else {
+                    segundo_acomp = prompt(`La selección es incorrecta. Elige una de estas opciones: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
@@ -89,46 +101,49 @@ if (choice == 'desayuno') {
         }
     }
 } else if (choice == 'cena') {
-    precio_total = 14.95
+    precio = 14.95
     for (var i=0; i <comida_cena.length; i++ ){
         if (i==0) {
             seleccion = true
+            var primero = prompt(`Los platos principales son: ${comida_cena[i][0]} o ${comida_cena[i][1]} y están incluidos dos acompañamientos por un precio total de ${precio}€.`)
             while (seleccion) {
-                var primero = prompt(`Los platos principales son: ${comida_cena[i][0]} o ${comida_cena[i][1]}`)
                 if(comida_cena[i].includes(primero)) {
                     seleccion = false
+                } else {
+                    primero = prompt(`Incorrecto. Elige una de estas opciones: ${comida_cena[i][0]} o ${comida_cena[i][1]} y están incluidos dos acompañamientos por un precio total de ${precio}€.`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
-            alert('El precio de la comida es 14,95€ con dos acompañamientos.')
         } else if (i==1){
             seleccion = true
+            var primer_acomp = prompt(`El primer acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
             while (seleccion) {
-                var primer_acomp = prompt(`El primer acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]}`)
                 if(comida_cena[i].includes(primer_acomp)) {
                     seleccion = false
+                } else {
+                    primer_acomp = prompt(`Tu selección no es correcta. Elige una de estas opciones: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
                 }
             } 
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
         } else {
             seleccion = true
+            var segundo_acomp = prompt(`El segundo acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
             while (seleccion) {
-                var segundo_acomp = prompt(`El segundo acompañamiento puede ser: ${comida_cena[i][0]} o ${comida_cena[i][1]}`)
                 if(comida_cena[i].includes(segundo_acomp)) {
                     seleccion = false
+                } else {
+                    segundo_acomp = prompt(`Tu selección no es correcta. Elige una de estas opciones: ${comida_cena[i][0]} o ${comida_cena[i][1]} y está incluido en el precio inicial de ${precio}€.`)
                 }
             }
             num_comentario = Math.floor(Math.random()*3)
             alert(comentarios[num_comentario])
         }
     }
-} else {
-    choice = prompt('Debes introudicr la opción como se escribe en la siguiente pregunta. Qué quieres desayuno, comida o cena?')
 }
 
-alert(`El precio total es de ${precio_total}€.`)
+alert(`El precio total es de ${precio}€ y has elegido ${primero}, ${primer_acomp} y ${segundo_acomp}. ¡Buen provecho!`)
 
 
 
